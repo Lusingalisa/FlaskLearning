8787ecd8b9c6418c0c936b326484d914cfdbdc68
Lisa Lusinga <lusingalisa7@gmail.com>
2025-09-20 14:33:19 +0300

    db creation and configurations

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template , url_for
+from flask import Flask, render_template , url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -12,7 +12,8 @@ db = SQLAlchemy(app)
 #creating a model
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.string(200), nullable=False)
+    content = db.Column(db.String(200), nullable=False)
+    completed = db.Column(db.Integer, default=0)
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
@@ -20,10 +21,23 @@ class Todo(db.Model):
 
 
 
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 # defining the function for the route we have just created.
 def index():
-    return render_template('index.html')
+    if request.method == 'POST':
+        task_content = request.form['content']
+        new_task = Todo(content=task_content)
+
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'There was an issue adding your task'
+
+    else:
+        tasks = Todo.query.order_by(Todo.date_created).all()
+        return render_template('index.html', tasks=tasks)
 
 if __name__ == "__main__":
     app.run(debug=True)
